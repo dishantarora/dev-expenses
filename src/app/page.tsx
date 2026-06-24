4ac795aed77648709b1a7656a35e3ee41be8ed10
Dishant Arora <dishantarora@outlook.com>
2026-06-24 10:55:28 +0530

    feat: initialize Next.js app with Tailwind CSS and TypeScript, remove boilerplate, add working React page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="flex flex-row justify-center item-center">
+        <p className="text-3xl font-semibold text-blue-400">Welcome</p>
+      </div>
+    </main>
+  );
+}
